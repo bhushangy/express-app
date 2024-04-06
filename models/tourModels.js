37aae6bbf22b6addf42bed3543d6f821094eab98
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-06 20:52:49 +0530

    MongoDB virual properties

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,48 +1,61 @@
 const mongoose = require('mongoose');
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const toursSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: { type: Number },
+    ratingsQuantity: { type: Number, default: 0 },
+    price: { type: Number, required: [true, 'A tour must have a price'] },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true,
+    },
+    // trim removes white space at the beginning and end of the string
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(), // If user does not send this, the default value will be inserted.
+      select: false, // Do not include this field in the api response.
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: { type: Number },
-  ratingsQuantity: { type: Number, default: 0 },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true,
-  },
-  // trim removes white space at the beginning and end of the string
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(), // If user does not send this, the default value will be inserted.
-    select: false, // Do not include this field in the api response.
-  },
-  startDates: [Date],
+);
+
+// You cannot use virtual properties in a query since it is not stores in db.
+// Thet are only added during get calls.
+toursSchema.virtual('durationWeeks').get(function () {
+  // this is poiting to the current document object.
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
