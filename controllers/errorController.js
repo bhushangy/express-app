8f5e370e1a4cd6934f4386b848f5d6d7b604031f
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-09 00:20:23 +0530

    refactored error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const errorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+};
+
+const errorProd = (err, res) => {
+    // Operational errors that are explicity thrown by developers.
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        // Programming or other unknown errors that are not marked as operational.
+        // Send minimum details to the client.
+        console.error('Error: ', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!!',
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === 'dev') {
+        errorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        errorProd(err, res);
+    }
 };
