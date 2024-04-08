2805376c5e662ccaef883560e9d8b40999cc2e0e
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-08 21:13:37 +0530

    error handling in each controller

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,6 @@
 // fn is an async function. So it returns a promise. Thus you can attach a catch hanlder to it.
 // We need not attach then handler because, all results are awaited inside async function.
+
+// This catch will catch both operational errors that you explicitly throw and programmatic errors
+// that mongoose or node or express might throw.
 module.exports = (fn) => (req, res, next) => fn(req, res, next).catch(next);
