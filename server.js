61628d94226a942a32da2ca02855a5bbcedb6c8a
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-09 14:43:10 +0530

    handling uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION');
+  console.log(err.name, err.message);
+  // Do we need to exit the process ??
+  // process.exit(1);
+});
+
 dotenv.config({ path: './.env' }); // This will read the data from the .env file and save then in the node process.env object
 const app = require('./app');
 
@@ -34,6 +41,15 @@ async function run() {
 run();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Express running on port ${port}!!`);
+});
+
+// Whenever there is an unhandled promise rejection, process object emits the below event.
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED PROMISE REJECTION');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
