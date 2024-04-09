36d86ddd33f82917d0e246d8b8e87e2b43485415
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-09 12:29:56 +0530

    mongoose error handling invalid user input

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
 const errorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -32,6 +39,8 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'dev') {
         errorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        errorProd(err, res);
+        let error = { ...err };
+        if (err.name === 'CastError') error = handleCastErrorDB(error);
+        errorProd(error, res);
     }
 };
