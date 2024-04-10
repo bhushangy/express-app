bdb55b1259b38549b43a2674b854c25285331d6e
Bhushan G Y <bhushan.benz@gmail.com>
2024-04-10 12:34:54 +0530

    added user schema

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,13 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/signup', signup);
+// Why 2 routes to create a user. /signup is for the actual user itself, whereas the other endpoints are
+// for admins who also may want to create users.
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
